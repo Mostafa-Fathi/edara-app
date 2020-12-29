52f3b5dc142fe140ea236bb9a5d56a0a20abd7f7
Mostafa-Fathi <mostafafathy677@gmail.com>
2020-12-29 03:56:56 +0200

    last edit

--- flutter_test_app/lib/main.dart
+++ flutter_test_app/lib/main.dart
@@ -24,6 +24,7 @@ class _MyHomePageState extends State<MyHomePage> {
       _obscureText = !_obscureText;
     });
   }
+
   var _formKey = GlobalKey<FormState>();
   TextEditingController userNameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
@@ -37,18 +38,15 @@ class _MyHomePageState extends State<MyHomePage> {
     debugPrint("$s");
     String _password;
 
-
     return Form(
-
-      key: _formKey,
+        key: _formKey,
         child: Directionality(
           textDirection: TextDirection.rtl,
-
           child: ListView(
             children: [
               logo(s),
               Padding(
-                  padding: EdgeInsets.only(right: 30, top: 20),
+                  padding: EdgeInsets.only(right: 20, top: 10),
                   child: Text(
                     "تسجيل الدخول ",
                     style: TextStyle(
@@ -56,11 +54,16 @@ class _MyHomePageState extends State<MyHomePage> {
                         color: const Color(0xff1341a7),
                         fontWeight: FontWeight.w900),
                   )),
-            usingImg("images/line.png", 20, 5),
-
               Padding(
-                  padding: EdgeInsets.only(
-                      top: 10, bottom: 10 ,right: 30,left: 30),
+                  padding: EdgeInsets.only(right: 20, top: 1, left: 280),
+                  child: Container(
+                    width: 2,
+                    height: 3,
+                    color: Colors.yellowAccent,
+                  )),
+              Padding(
+                  padding:
+                      EdgeInsets.only(top: 10, bottom: 10, right: 20, left: 30),
                   child: TextFormField(
                       keyboardType: TextInputType.emailAddress,
                       controller: userNameController,
@@ -72,14 +75,17 @@ class _MyHomePageState extends State<MyHomePage> {
                       },
                       decoration: InputDecoration(
                           labelText: 'اسم المستخدم ',
+                          suffixIcon: Icon(Icons.create,
+                              color: const Color(0xff1341a7)),
+                          prefixIcon: Icon(Icons.confirmation_num_outlined),
                           labelStyle: textStyle,
                           errorStyle:
-                          TextStyle(color: Colors.red, fontSize: 15.0),
+                              TextStyle(color: Colors.red, fontSize: 15.0),
                           border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(5.0))))),
               Padding(
-                  padding: EdgeInsets.only(
-                      top: 10, bottom: 10 ,right: 30,left: 30),
+                  padding:
+                      EdgeInsets.only(top: 10, bottom: 10, right: 20, left: 30),
                   child: TextFormField(
                       keyboardType: TextInputType.visiblePassword,
                       controller: passwordController,
@@ -87,23 +93,72 @@ class _MyHomePageState extends State<MyHomePage> {
                       onSaved: (val) => _password = val,
                       obscureText: _obscureText,
                       validator: (String value) {
-                        if (value.isEmpty&&value.length<6) {
-                          return 'كلمة المرور ضعيفه   ';
+                        if (value.isEmpty && value.length < 6) {
+                          return 'ادخل كلمة المرور    ';
                         }
                       },
                       decoration: InputDecoration(
-                          icon: const Padding(
-                              padding: const EdgeInsets.only(top: 15.0),
-                              child: const Icon(Icons.lock)),
+                          suffixIcon: GestureDetector(
+                            child: Icon(Icons.remove_red_eye_sharp,
+                                color: const Color(0xff1341a7)),
+                            onTap: () {
+                              _toggle();
+                            },
+                          ),
+                          prefixIcon: Icon(Icons.lock_outline),
                           labelText: 'كلمة المرور  ',
                           labelStyle: textStyle,
                           errorStyle:
-                          TextStyle(color: Colors.red, fontSize: 15.0),
+                              TextStyle(color: Colors.red, fontSize: 15.0),
                           border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(5.0))))),
-              new FlatButton(
-                  onPressed: _toggle,
-                  child: new Text(_obscureText ? "Show" : "Hide")),
+              Padding(
+                padding:
+                    EdgeInsets.only(top: 10, bottom: 10, right: 20, left: 30),
+                child: Row(
+                  children: [
+                    GestureDetector(
+                      child: Text(
+                        "نسيت كلمة المرور؟",
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: const Color(0xff1341a7),
+                            fontWeight: FontWeight.w200),
+                      ),
+                      onTap: () {
+                        debugPrint("نسيت كلمة المرور ");
+                      },
+                    ),
+                    Container(width: 80,),
+                    GestureDetector(
+                      child: Text(
+                        "ليس لديك حساب ؟",
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: const Color(0xff1341a7),
+                            fontWeight: FontWeight.w200),
+                      ),
+                      onTap: () {
+                        debugPrint("ليس لديك حساب ");
+                      },
+                    )
+                  ],
+                ),
+              ),
+              Padding(padding:EdgeInsets.only( right: 280, left: 30) ,child:RotationTransition(  turns: new AlwaysStoppedAnimation(180 / 360),
+
+                child : FloatingActionButton(
+                  onPressed: () {
+                    debugPrint('FAB clicked');
+
+                  },
+
+                  child: Icon(Icons.login_rounded,color:Colors.white,),
+                  backgroundColor: Colors.yellow,
+
+
+                ),
+              ))
 
             ],
           ),
@@ -118,7 +173,8 @@ Widget logo(double c) {
     height: 250,
     width: c,
     decoration: BoxDecoration(
-        image: DecorationImage(image: AssetImage('images/header.png'))),
+        image: DecorationImage(
+            fit: BoxFit.fill, image: AssetImage('images/header.png'))),
     child: Center(
       child: Container(
         child: image,
@@ -128,7 +184,8 @@ Widget logo(double c) {
     ),
   );
 }
-Widget usingImg(String loction ,double w,double h) {
+
+Widget usingImg(String loction, double w, double h) {
   AssetImage assetImage = AssetImage(loction);
   Image image = Image(image: assetImage);
   return Container(
